ef89406bd21d80806275db9f83811318cb106335
DiegoAmaya19 <141657295+DiegoAmaya19@users.noreply.github.com>
2025-09-17 20:44:01 -0500

    ddd

---
+++ HR-JPA/hr-jpa/src/main/java/com/devsenior/diego/hr_jpa/HrJpaApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.diego.hr_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrJpaApplication.class, args);
+	}
+
+}
---
+++ HR-JPA/hr-jpa/src/test/java/com/devsenior/diego/hr_jpa/HrJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.diego.hr_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
